94c1ac6e108f3627b3089997d700b93d2d284ace
keeley.gibbs@yahoo.com <keeley.gibbs@yahoo.com>
2019-09-19 01:16:49 +0000

    Pagination

--- Gemfile
+++ Gemfile
@@ -71,4 +71,8 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
+gem 'will_paginate', '> 3.0'
+
 gem 'simple_form'
+
+
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,11 @@
 class PlacesController < ApplicationController
+    
     def index
-      @places = Place.all
+        @places = Place.order("name").page(params[:page]).per_page(2)
     end
-  
+
     def new
         @place = Place.new
     end
   
-  end
+  end 
