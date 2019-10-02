2744cc9c4f8dd7d1502f7d18a67cc32fece694d0
keeley.gibbs@yahoo.com <keeley.gibbs@yahoo.com>
2019-10-02 23:28:01 +0000

    geolocator

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,8 @@
 class Place < ApplicationRecord
     belongs_to :user
+    geocoded_by :address
+    after_validation :geocode
+    validates :name, presence: true, length: { in: 3..50}
+    validates :address, presence: true, length: { minimum: 5}
+    validates :description, presence: true, length: { in: 5..1000}
 end
