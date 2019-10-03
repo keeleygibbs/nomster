66e08b0a5011178afc39a39774c02936d39a085f
keeley.gibbs@yahoo.com <keeley.gibbs@yahoo.com>
2019-10-03 00:51:46 +0000

    setting up comment database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
     belongs_to :user
+    has_many :comments
+
     geocoded_by :address
     after_validation :geocode
+    
     validates :name, presence: true, length: { in: 3..50}
     validates :address, presence: true, length: { minimum: 5}
     validates :description, presence: true, length: { in: 5..1000}
